f9e25f6fc92fe6d2e03dbb8e2939a3dc50ed05b3
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2022-12-27 20:13:16 -0800

    Update Redundant Connection.py

--- graph/union-find-problems/Redundant Connection.py
+++ graph/union-find-problems/Redundant Connection.py
@@ -1,1 +1,45 @@
 
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+
+        n = len(edges)+1
+        root = [i for i in range(n)]
+        rank = [1] * n
+        ans = []
+        
+
+
+        def find(x):
+            if x == root[x]:
+                return x
+            root[x]= find(root[x])
+            return root[x]
+
+
+        def union(x,y):
+
+            rootX = find(x)
+            rootY = find(y)
+
+            if rootX != rootY:
+                if rank[rootX]> rank[rootY]:
+                    root[rootY]=rootX
+
+                elif rank[rootY]> rank[rootX]:
+                    root[rootX] = rootY
+                    
+                else:
+                    root[rootY]=rootX
+                    rank[rootX]+=1
+                
+            else:
+                return True
+         
+
+        for i,j in edges:
+            
+            if union(i,j):
+                ans = [i,j]
+                break
+
+        return ans        
